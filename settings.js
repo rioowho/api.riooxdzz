baaa3f82246c281a2928236c7ab85587cf0f43fa
rioowho <riooishere>
2024-11-15 20:09:56 +0700

    api.riooxdzz

---
+++ settings.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+global.creator = 'RiooXdzz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://riooxdzz:qIVc4p12eTGc1hZ4@cluster0.j6pqf.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "riooajahh211@gmail.com" //email
+global.email_password = "dgygqhgczzpomqty" //application password email
+global.limitCount = 20
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
